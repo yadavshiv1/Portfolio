0e771fe8151966d0f7e123ec855bddd874973fce
Apple <apple@Apples-MacBook-Air-2.local>
2022-08-25 15:45:55 +0530

    Updates

---
+++ precache-manifest.586288b36c639f918703246d5ec78600.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a310164b266cfaab6d2ae0a15700eec4",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "8cd0c6171c24ec59c434",
+    "url": "/Portfolio/static/css/main.4562c532.chunk.css"
+  },
+  {
+    "revision": "f7a155a0d8dc2294b13f",
+    "url": "/Portfolio/static/js/2.7104d1fb.chunk.js"
+  },
+  {
+    "revision": "1fc948039653eaaa0582d4f0d087fe95",
+    "url": "/Portfolio/static/js/2.7104d1fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd0c6171c24ec59c434",
+    "url": "/Portfolio/static/js/main.4ed57d73.chunk.js"
+  },
+  {
+    "revision": "993bcc0e93b144c43bcd",
+    "url": "/Portfolio/static/js/runtime-main.6579afd0.js"
+  },
+  {
+    "revision": "75abe7aef94e06eddc2e6470abf2eb1d",
+    "url": "/Portfolio/static/media/photo.75abe7ae.jpeg"
+  }
+]);
